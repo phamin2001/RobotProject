9d73b8f4c46c97f6c70352ba6b1276d68ba1ed0d
phamin2001 <amin.pahlavani@gmail.com>
2019-09-05 16:41:56 -0600

    mail truck route

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Our project is to build an automaton, a little program that performs a task in a virtual world. Our automaton will be a mail-delivery robot picking up and dropping off parcels.
+
+There’s the robot’s current location and the collection of undelivered parcels, each of which has a current location and a destination address. That’s it.
+And while we’re at it, let’s make it so that we don’t change this state when the robot moves but rather compute a new state for the situation after the move.
+
--- app.js
+++ app.js
@@ -45,7 +45,7 @@ class VillageState {
 
   move(destination) {
     if (!roadGraph[this.place].includes(destination)) {
-      return this; //return old state
+      return this;
     } else {
       let parcels = this.parcels
         .map(p => {
@@ -62,11 +62,76 @@ class VillageState {
 let first = new VillageState('Post Office', [
   { place: 'Post Office', address: "Alice's House" }
 ]);
-console.log(first);
+// console.log(first); // show a state
 
 console.log('/////After Move/////');
 let next = first.move("Alice's House");
-console.log(next);
-// console.log(next.place);
-// console.log(next.parcels)
-console.log(first.place);
+// console.log(next);
+
+function runRobot(state, robot, memory) {
+  console.log(`memory is: `, memory);
+
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.place]) };
+}
+
+VillageState.random = function(parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place == address);
+    parcels.push({ place, address });
+  }
+  return new VillageState('Post Office', parcels);
+};
+
+// start up a virtual world
+// runRobot(VillageState.random(), randomRobot);
+// console.log(VillageState.random());
+
+// add memory to get better result
+const mailRoute = [
+  "Alice's House",
+  'Cabin',
+  "Alice's House",
+  "Bob's House",
+  'Town Hall',
+  "Daria's House",
+  "Ernie's House",
+  "Grete's House",
+  'Shop',
+  "Grete's House",
+  'Farm',
+  'Marketplace',
+  'Post Office'
+];
+
+function routeRobot(state, memory) {
+  if (memory.length == 0) {
+    memory = mailRoute;
+  }
+  return { direction: memory[0], memory: memory.slice(1) };
+}
+
+runRobot(VillageState.random(), routeRobot, (memory = mailRoute));
